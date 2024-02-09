2d4b8399f7938dbe39aad96e6965177343d04b11
Dejorden <d.moerman@hotmail.nl>
2024-02-09 13:06:44 +0100

    declare list element in javascript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const button = document.getElementById('js--submit');
 const input = document.getElementById('js--input');
 
+const list = document.getElementById('js--todo-list');
+
 button.addEventListener('click', () => {
     const inputValue = input.value;
     console.log(inputValue);
     input.value = '';
+    if (inputValue) {
+
+    }
 });
