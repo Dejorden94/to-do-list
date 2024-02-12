c7144f896e245fc0fa76c1a9f5ffaaad8979bf0d
Dejorden <d.moerman@hotmail.nl>
2024-02-12 13:38:10 +0100

    add function to remove items from to do list

--- js/main.js
+++ js/main.js
@@ -13,8 +13,8 @@ button.addEventListener('click', () => {
     return alert('Voeg een item toe!')
 });
 
-let setToDoData = (input) => {
-    //Kijk of item al bestaad in de localStorage en stuurd truw wanneer dit zo is.
+const setToDoData = (input) => {
+    //Kijk of item al bestaad in de localStorage en stuurt true wanneer dit zo is.
     if (getData(input) != false) {
         return alert('Item al toegevoegd aan todo');
     }
@@ -25,13 +25,11 @@ let setToDoData = (input) => {
     return localStorage.setItem('toDoList', data);
 };
 
-let getData = (input = null) => {
+const getData = (input = null) => {
     let data = JSON.parse(localStorage.getItem('toDoList'));
-    console.log(data);
     if (data) { //Als er data is voor de onderstaande functie uit.
         if (input) { //Als de gebruiker input heeft gegeven voor de onderstaande functie uit.
             if (data.indexOf(input) != -1) {
-                console.log(data.indexOf(input));
                 return data[input];
             } else {
                 return false;
@@ -42,15 +40,29 @@ let getData = (input = null) => {
     return false;
 }
 
-let listToDo = () => {
+const listToDo = () => {
     let list = document.getElementById('js--todo-list');
     let data = getData();
     let todos = ``
     if (data) {
-        data.forEach((value, item) => {
+        data.forEach((value, item) => { //Pak de waarde en de index van het object en maakr nieuwe li's aan.
             todos += `<li>${value}&nbsp; &nbsp; &nbsp; <button onclick="removeData(${item})">Verwijder</button></li>`
         });
-        list.innerHTML = todos;
+        list.innerHTML = todos;// Set de list zijn html  gelijk aan de todos variable waarde
+    }
+}
+
+const removeData = (itemId) => {
+    let data = getData(); //Haal data op uit local storage.
+    if (data) {
+        let newData = data.filter((current, index) => {
+            return index != itemId //Voeg het element toe wanneer de index niet overeenkomt met het gegeven itemId.
+        });
+        newData = JSON.stringify(newData);
+        localStorage.setItem('toDoList', newData); //Set de waarde van het toDolist item om naar de waarde van de newData array.
+        listToDo();
+    } else {
+        alert("no data found");
     }
 }
 
