6a8548dafe034e72b01d179756ceef4a55511034
Dejorden <d.moerman@hotmail.nl>
2024-02-12 12:54:19 +0100

    add function for displaying to do's

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ button.addEventListener('click', () => {
     const inputData = input.value;
     if (inputData != '') {
         return setToDoData(inputData);
-        // listToDo();
+        listToDo();
     }
     return alert('Voeg een item toe!')
 });
@@ -20,6 +20,34 @@ let setToDoData = (input) => {
     let data = getData();
     data = (data != false) ? data : []; //Als data niet bestaad, maak een lege aarray ndata aan.
     data.push(input); //Voeg het ingevoerde item toe aan de data array.
-    data.JSON.stringify(data);
+    data = JSON.stringify(data);
     return localStorage.setItem('toDoList', data);
 };
+
+let getData = (input = null) => {
+    let data = JSON.parse(localStorage.getItem('toDoList'));
+    console.log(data);
+    if (data) {
+        if (input) {
+            if (data.indexOf(input) != -1) {
+                return data[input];
+            } else {
+                return false;
+            }
+        }
+        return data;
+    }
+    return false;
+}
+
+let listToDo = () => {
+    let list = document.getElementById('js--todo-list');
+    let data = getData();
+    let todos = ``
+    if (data) {
+        data.forEach((value, item) => {
+            todos += `<li>${value}&nbsp; &nbsp; &nbsp; <button onclick="removeData(${item})">Verwijder</button></li>`
+        });
+        list.innerHTML = todos;
+    }
+}
